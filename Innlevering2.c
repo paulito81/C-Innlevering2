a41edc5d8f637ef327b9fa2fce2eed3bf9a3e022
Paul Hasfjord <phasfjo@online.no>
2015-10-31 20:01:07 +0100

    alfa check and DEFINE Max, Filename check

--- Innlevering2.c
+++ Innlevering2.c
@@ -1,9 +1,11 @@
 // Innlevering 2 by Paul Hasfjord
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "secretCoder.h"
 #include "fileList.h"
 
+#define MAXFILE_SIZE 20000
 
 
 
@@ -13,15 +15,20 @@ char *encode(const char *inputMessageFile, const char *keyFile, int *status);
 int encode2(const char *inputMessageFile, const char *keyFile, char *encodedStream);
 char *decode(const char *inputCodeFile, const char *keyFile, int status);
 int decode2(const char *inputCodeFile, const char *keyFile, char *decodedMessage);
-/*
-void mallocBuffer(char *ptr);
-void free(void *pointer);
-*/
-int main(){
 
-		char fileName[1000] = "";
-		strcat(fileName, FILENAME4);
-		readFromFile(fileName, "r");
+bool alphabetic( const char a);
+int countWords( const char string[]);
+
+
+//void *malloc(size_t size);
+//void free(void *pointer);
+
+int main(){
+	//	char FileName[100] = "";
+	//	printf("Welcome to Encoder/decoder!\tChoose a file '1-121' to decode\n(Exp: FILENAME1 = (filename1), FILENAME2 = fileName2 .. )\n");
+//		scanf("%s", FileName);
+		readFromFile(FILENAME50 , "r");
+//		readFromFile( FILENAME1, "r");
 		
 	/*
 		TODO Encoding and Decoding reverse process 
@@ -48,58 +55,94 @@ int main(){
 	
 		
 }
+
+void readLine(char buffer[] ){
+	char character;
+	int i = 0;
+
+	do{
+		character = getchar ();
+		buffer[i] = character;
+		++i;
+	}
+
+	while( character != '\n' );
+
+	buffer[i-1] = '\0';
+}
+bool alphabetic( const char a){
+	if( (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') )
+		return true;
+	else
+		return false;	
+}
+int countWords( const char string[]){
+	int countWords = 0;
+	bool lookingForward = true, alphabetic( const char a);
+
+	for(int i = 0; string[i] != '\0'; ++i)
+		if( alphabetic( string[i]) )
+		{
+		  if( lookingForward){
+		  	++countWords;
+		  	lookingForward = false;
+		  }	
+		}
+		else
+			lookingForward = true;
+		return countWords;
+}
 int readFromFile(const char fileName[], const char *mode) {
 		FILE *fp;
-		int c ;
-
+		int index;
+		int i = 0;
+	
+		char c[MAXFILE_SIZE] ;
+	
 		fp = fopen(fileName, mode);
 		if( fp == NULL){
-			perror("Error in opening file");
+			perror("Unable to open the file.\n(Hint: try open an another file..)");
 			return(-1);
 		}
 		printf("\n");
 
-		while(1){
+		do{
 			//keyFile = fgetc(fp);
 
-			c = fgetc(fp);
-			if( feof(fp) ){
+			index = fgetc(fp);
+			c[i] = index;
+			++i;
+			
+		/*	if( index == '\0'){
 				break;
 			}
-				printf("%c", c);
+		
+		*/	if( feof(fp) ){
+				c[i-1] = index;
+				break;
+			}
+		//	printf("%c", c[i]);
 				// printf("[%3i] ", c);
-		
-			// printf("[%3c] ", c);
-		}
-		
-		//printf("%d\n", keyFile);
-
+				// printf("[%3c] ", c);
+		}while( 1);
 		printf("\n");
-		//printf("%d\n", sizeOf(keyFile));
-
-	  /*
-		char mystring [10000];
+		for( int j = 0; j < 840; j++){
+			printf("%c", c[j] );
+		}
+		printf("\n");
+			printf("%d words\n", countWords(c));
 	
-	   	fp = fopen(fileName, mode);
-		if (fp == NULL) perror ("Error opening file");
-		else {
-		if ( fgets (mystring , 10000 , fp) != NULL )
-		puts (mystring);
-	}	
-	*/
 		fclose(fp);
 	
 	return 0;
 
 }
 /*
-void mallocBuffer(char *ptr){
-	(int)ptr = malloc(10* sizeOf(*ptr));	
+void *malloc(size_t size){
+	 size = (int *) malloc(100* sizeof (int) );
 }
 void free(void *pointer){
-	pointer.free();
+	free(pointer);
 }
+
 */
-
-
-
